e1423d4e23ddfcacc68a14a8132ffd4805c0f1b3
Artur <arturunkulov30@gmail.com>
2024-07-26 18:24:37 +0600

    new repozitori

---
+++ test.py
@@ -0,0 +1,13 @@
+
+
+
+
+#######  1  git init (Гитти инисиялизация кылуу)
+######## 2 git add .
+ #######   3 git commit -m "first commit"
+ ########   4 git branch -M main
+ ######### 5 git remote add origin https://github.com/Artur470/git-lesson-new-.git
+ ######## 6 git push -u origin main
+
+
+
